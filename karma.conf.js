289969f4b744c92c1a6b3ff65da0da8026c9103b
AdiletMaksatuly <bakeryamaker1@gmail.com>
2023-01-22 15:52:37 +0500

    docs: change project name

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/project-management-app'),
+      dir: require('path').join(__dirname, './coverage/teamwork'),
       subdir: '.',
       reporters: [
         { type: 'html' },
